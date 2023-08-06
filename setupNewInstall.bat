a6aa64f6e4d00b8e50f2c558b2409edd975e66d9
Fox's Den <47910472+FoxsDenYT@users.noreply.github.com>
2023-08-06 20:02:31 +1000

    Create setupNewInstall.bat

---
+++ setupNewInstall.bat
@@ -0,0 +1,34 @@
+echo THE ULTIMATE SETUP
+echo installing...
+winget install -e --id Google.Chrome.Dev
+winget install -e --id Microsoft.WindowsTerminal
+winget install -e --id PuTTY.PuTTY
+winget install -e --id VSCodium.VSCodium
+winget install -e --id Microsoft.VisualStudio.2022.Community.Preview
+winget install -e --id Spotify.Spotify
+winget install -e --id Discord.Discord
+winget install -e --id Obsidian.Obsidian
+winget install -e --id Google.Drive
+winget install -e --id RustemMussabekov.Raindrop
+winget install -e --id Git.Git
+winget install -e --id GitHub.GitHubDesktop
+winget install -e --id valinet.ExplorerPatcher
+winget install -e --id AutoHotkey.AutoHotkey
+winget install -e --id infinitepower18.WSASideloader
+winget install -e --id KDE.KDEConnect
+winget install -e --id Microsoft.PowerToys
+winget install -e --id ShareX.ShareX
+winget install -e --id Datronicsoft.SpacedeskDriver.Server
+winget install -e --id WhatsApp.WhatsApp
+winget install -e --id ByteDance.CapCut
+winget install -e --id OBSProject.OBSStudio
+winget install -e --id VB-Audio.Voicemeeter.Banana
+winget install -e --id Apple.iTunes
+winget install -e --id AnyDeskSoftwareGmbH.AnyDesk
+winget install -e --id Audacity.Audacity
+echo 
+echo 
+echo 
+echo
+echo Installed!
+pause
